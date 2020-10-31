dd04628e0630aec888507e22ff1579c860263525
projectguide-milestone <projectguidemilestone@gmail.com>
2020-10-31 17:16:52 +0530

    navigation from app.js

--- UserAccount/myuseraccount/src/App.js
+++ UserAccount/myuseraccount/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {component} from 'react';
 import './App.css';
+import {loginPage} from './components/loginPage';
+import {Navigation} from './components/navigation';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <BrowserRouter>
+      <div classname="container">
+        <Navigation/>
+        <Switch>
+          <Route path='/' component={loginPage} exact></Route>
+        </Switch>
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
