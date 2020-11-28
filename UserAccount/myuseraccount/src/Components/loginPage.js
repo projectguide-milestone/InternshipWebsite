ca1aee0dc2f1a343b2c9f120d8ec069771fee88a
projectguide-milestone <projectguidemilestone@gmail.com>
2020-11-28 14:33:18 +0530

    Changes on loginpage

--- UserAccount/myuseraccount/src/Components/loginPage.js
+++ UserAccount/myuseraccount/src/Components/loginPage.js
@@ -5,7 +5,6 @@
 // on failure give appropriate message
 //Plus links to registration and forgot password link
 //document
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import {Navbar, Nav} from 'react-bootstrap';
@@ -17,6 +16,3 @@ export class loginPage extends Component{
         )
     }
 }
-=======
-//basic format for file
->>>>>>> 3be50ce9991be1f05ad574e82207cfb1f09edd24
