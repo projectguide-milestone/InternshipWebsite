acadf1f79320d3f3290bc43ed5140efae803c568
projectguide-milestone <projectguidemilestone@gmail.com>
2021-01-09 15:24:50 +0530

    Update: removed unnecessary page

--- UserAccount/myuseraccount/src/Components/registrationPage.js
+++
@@ -1,6 +0,0 @@
-//Registration
-//Module 1 done by Mohammad and Ratan
-//Linked from Login page
-//Asks user to create a new account. Ask for appropriate details
-//On success link back to login page
-//some change
